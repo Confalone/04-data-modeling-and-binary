b68c7cfe56a8d97bdb41c99517445c347be51aa2
Tyler Confalone <tconfalone@gmail.com>
2018-07-08 16:46:53 -0700

    Travis maintence

--- __test__/app.test.js
+++ __test__/app.test.js
@@ -2,9 +2,6 @@
 
 let bitReader = require('../src/app.js');
 let BufferData = require('../src/lib/bitmap');
-let transformer = require('../src/lib/transformation');
-// let index = require('../index');
-
 
 describe('Bitmap reader', () => {
 
@@ -12,25 +9,22 @@ describe('Bitmap reader', () => {
     let filePath = 'assets/missing.txt';
     bitReader.readFile(filePath, (err) => {
       expect(err).not.toBeNull();
-      // console.log(buffer, 'buffer');
       done();
     });
   });
 
-
   it(' should call on buffer when given a valid file path', (done) => {
-    let filePath = 'assets/bitmap.bmp';//?
-    bitReader.readFile(filePath, (err, buffer) => {//?
+    let filePath = 'assets/bitmap.bmp';
+    bitReader.readFile(filePath, (err, buffer) => {
       expect(err).toBeNull();
       expect(typeof buffer).toBe('object');
-      console.log(buffer.length);//?
       done();
     });
   });
 
 
   it(' should call the first two chars of the buffer and they should be BM', (done) => {
-    let filePath = __dirname + '/../assets/bitmap.bmp';//?
+    let filePath = __dirname + '/../assets/bitmap.bmp';
     bitReader.readFile(filePath, (err, buffer) => {
       let actual = buffer.toString('utf-8', 0, 2);
       let expected = 'BM';
@@ -41,12 +35,10 @@ describe('Bitmap reader', () => {
 
 
   it(' should show an object with the correct dimensions', (done) => {
-    let filePath = __dirname + '/../assets/bitmap.bmp';//?
+    let filePath = __dirname + '/../assets/bitmap.bmp';
     bitReader.readFile(filePath, (err, buffer) => {
       expect(err).toBeNull();
       let results = new BufferData(buffer);
-      // console.log(results.colorPalette);
-      // console.log('table', results.colorPalette.length);//?
       expect(results.width).toBe(100);
       expect(results.height).toBe(100);
       done();
@@ -58,27 +50,10 @@ describe('Bitmap reader', () => {
     bitReader.readFile(filePath, (err, buffer) => {
       if (err) console.log(err);
       expect(err).toBeNull();
-      bitReader.writeFile(__dirname + '/../assets/changed/newFile.bmp', buffer, (err, buffer) => {
+      bitReader.writeFile(__dirname + '/../assets/changed/newFile.bmp', buffer, (err, buffer) => { // eslint-disable-line
         expect(err).toBeNull();
         done();
       });
     });
   });
-
-  it(' should transform a bitmap', (done) => {
-    let filePath = __dirname + '/../bitmap.bmp';
-    bitReader.readFile(filePath, (err, buffer) => {
-      if (err) console.log(err);
-      expect(err).toBeNull();
-      let testing = new BufferData(buffer);
-      transformer(testing, 'invert', (err, buffer) => {
-        expect(err).toBeNull();
-        // expect(buffer).toBe.length() 
-        // expect()
-        done();
-      });
-    });
-  });
-
-
 });
